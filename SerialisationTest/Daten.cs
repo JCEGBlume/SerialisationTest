ae7aa6a4972e01bf1139fd14743989777cc5e5d2
Zirrrus <zirrrus@gmail.com>
2020-02-05 21:30:06 +0100

    Projektdateien hinzufügen.

---
+++ SerialisationTest/Daten.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerialisationTest
+{
+    [Serializable]
+    class Daten
+    {
+        public string text;
+
+        public Daten(string text)
+        {
+            this.text = text;
+        }
+    }
+}
---
+++ SerialisationTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace SerialisationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input: ");
+            string input = Console.ReadLine();
+
+            Daten daten = new Daten(input);
+
+            FileStream fileStreamBin = new FileStream("test.bin", FileMode.OpenOrCreate);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fileStreamBin, daten);
+            fileStreamBin.Close();
+
+            FileStream fileStreamXml = new FileStream("test.xml", FileMode.OpenOrCreate);
+            XmlSerializer xmlf = new XmlSerializer(typeof(Daten));
+            xmlf.Serialize(fileStreamXml, daten);
+            fileStreamXml.Close();
+
+            FileStream fileStreamXmlRead = new FileStream("test.xml", FileMode.Open);
+            Daten datenRead = (Daten)xmlf.Deserialize(fileStreamXmlRead);
+            fileStreamXmlRead.Close();
+        }
+    }
+}
